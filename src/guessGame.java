42f44dc6d6862c67c58c7315803848c52b45ca2d
Abdul Rafi <arafi6624@bths.edu>
2018-10-11 14:48:44 -0400

    Completed 1 extension.

--- src/guessGame.java
+++ src/guessGame.java
@@ -4,18 +4,27 @@ public class guessGame {
     public static void main(String[] args){
         double x = Math.random();
         int num = (int)(x * 101);
+        int tracker = 0;
+        Scanner name = new Scanner(System.in);
+        System.out.println("Welcome to the guess game! Please enter your name to begin.");
+
         Scanner input = new Scanner(System.in);
-        String guess = "";
         System.out.println("Try to guess the number!");
-        guess = input.nextLine();
+        int guess = input.nextInt();
+        tracker ++;
+        while (num != guess){
             if (guess < num){
                     System.out.println("Guess higher!");
-                    guess = input.nextLine();
+                    guess = input.nextInt();
+                    tracker++;
             }
             else{
                     System.out.println("Guess lower");
-                    guess = input.nextLine();
+                    guess = input.nextInt();
+                    tracker++;
                 }
         }
+        System.out.println("You got it! The number is: " + num);
+        System.out.println("It took you " + tracker + " tries to guess the number!");
     }
 }
